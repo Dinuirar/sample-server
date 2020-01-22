f68cefa1acd502f94c45967e026c55a6610fd595
dinuirar <dinuirar@gmail.com>
2020-01-22 18:49:45 -0500

    added preliminary database info

---
+++ sample-tm-db/table-project.sql
@@ -0,0 +1,5 @@
+Telemetry:
+id <int> | luminosity <double> | humidity <double> | temperature <double> | pressure <double> | lights <short integer> | airfan <boolean> | heater <boolean> | timestamp <datetime>
+
+Telecommands:
+id <int> | telecommand <varchar(300)> | timestamp <datetime>
